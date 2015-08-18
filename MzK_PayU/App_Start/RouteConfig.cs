f39ea860b0ac82e11fd36a64fbf28636c381bd07
mtztkt <mtztkt@gmail.com>
2015-08-18 10:53:14 +0300

    Payu rest api
    rest api

---
+++ MzK_PayU/App_Start/RouteConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MzK_PayU
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Home.Index", "", new { controller = "Home", action = "Index" });
+            routes.MapRoute("Home.Payment", "odeme", new { controller = "Home", action = "Payment" });
+            routes.MapRoute("Home.PaymentThanks", "odeme-tesekkur", new { controller = "Home", action = "PaymentThanks" });
+        }
+    }
+}
